30c24f05cff7682ce673a0f0b64b5c9e4774ff8b
Danilo Dorotheu <danilo.dorotheu@live.com>
2016-07-31 20:25:23 -0300

    - Incluindo o metodo update em ClientController; - Finalizando o projeto 1;

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -9,6 +9,7 @@ use App\Http\Requests;
 class ClientController extends Controller
 {
     /**
+     * Exibe todos os registros
      * 
      * @return Response
      */
@@ -28,13 +29,16 @@ class ClientController extends Controller
     }
     
     /**
+     * Atualiza um registro
      * 
      * @param Request $request
      * @param int $id
      * @return Response
      */
     public function update(Request $request, $id) {
-        
+        $client = \App\Client::find($id);
+        $client->update($request->all());
+        return $client;
     }
     
     /**
@@ -48,12 +52,15 @@ class ClientController extends Controller
     }
     
     /**
-     * Retorna um registro específico
+     * Remove um registro
      * 
      * @param int $id
      * @return Response
      */
     public function destroy($id) {
-        \App\Client::find($id)->delete();
+        $client = \App\Client::find($id);
+        $nomeCliente = $client->name;
+        $client->delete();
+        return "Cliente {$nomeCliente} foi removido!";
     }
 }
